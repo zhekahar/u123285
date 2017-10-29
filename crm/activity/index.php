34b279d627969186574f412a7da29593fe5bfea0
Evgeny Haritonov <zhekahar@rambler.ru>
2017-10-29 11:32:47 +0300

    git-repository creation

---
+++ crm/activity/index.php
@@ -0,0 +1,25 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+IncludeModuleLangFile($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/intranet/public/crm/activity/index.php");
+$APPLICATION->SetTitle(GetMessage("CRM_TITLE"));
+if (CModule::IncludeModule("crm"))
+{
+?>
+<?$APPLICATION->IncludeComponent("bitrix:crm.activity.list",
+	"grid",
+	array(
+		'PERMISSION_TYPE' => 'WRITE',
+		'ENABLE_TOOLBAR' => true,
+		'DISPLAY_REFERENCE' => true,
+		'DISPLAY_CLIENT' => true,
+		'AJAX_MODE' => 'Y',
+		'AJAX_OPTION_JUMP' => 'N',
+		'AJAX_OPTION_HISTORY' => 'N',
+		'PREFIX' => 'MY_ACTIVITIES',		
+	),
+	false
+);?>
+<?
+}
+?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
